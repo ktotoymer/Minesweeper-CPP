6abd78f3e5609950dfb6b6f44f7ca3917a4f7126
ktotoymer <mishaaalexeev@yandex.ru>
2024-05-21 22:16:39 +0300

    new init

---
+++ MineSweeper.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Mikhail Alexeev on 21.05.2024.
+//
+
+#include "MineSweeper.h"
+
+namespace std {
+} // std
---
+++ MineSweeper.h
@@ -0,0 +1,16 @@
+//
+// Created by Mikhail Alexeev on 21.05.2024.
+//
+
+#ifndef MINESWEEPER_CPP_MINESWEEPER_H
+#define MINESWEEPER_CPP_MINESWEEPER_H
+
+namespace std {
+
+    class MineSweeper {
+
+    };
+
+} // std
+
+#endif //MINESWEEPER_CPP_MINESWEEPER_H
